4a486dda2e180447c9a9fa70b098f77be04cf81a
Ahmad Siddiq <ahmadsiddiqpali@mail.com>
2020-02-29 14:39:49 +0700

    add bookedlistroom

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const room = require("./room");
 const listroom = require("./listroom");
+const oreders = require('./orders');
 
 const Router = express.Router();
 
 Router.use("/room", room);
 Router.use("/listroom", listroom);
+Router.use("/orders", oreders);
 
 module.exports = Router;
