1a8d60471536d9b8ec66d793a43c87080590c191
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-02-29 22:43:24 +0700

    fix

--- src/router/listroom.js
+++ src/router/listroom.js
@@ -1,17 +1,15 @@
-const Express = require('express');
-const Multer = require('multer');
+const Express = require("express");
+const Multer = require("multer");
 
 const Router = Express.Router();
-const listRoomController = require('../controllers/listroom');
+const listRoomController = require("../controllers/listRoom");
 
-
-Router
-.get('/:idRoom', listRoomController.getlistRoom)
-.get('/search/:data', listRoomController.searchlistRoom)
-.get('/sort/:data', listRoomController.sortRoom)
-.get('/pagination/:page', listRoomController.paginationlistRoom)
-.delete('/delete/:id', listRoomController.deletelistRoom)
-.post('/insertlistroom', listRoomController.insertlistRoom)
-.put('/updatelistroom/:id', listRoomController.updatelistRoom)
+Router.get("/:idRoom", listRoomController.getlistRoom)
+  .get("/search/:data", listRoomController.searchlistRoom)
+  .get("/sort/:data", listRoomController.sortRoom)
+  .get("/pagination/:page", listRoomController.paginationlistRoom)
+  .delete("/delete/:id", listRoomController.deletelistRoom)
+  .post("/insertlistroom", listRoomController.insertlistRoom)
+  .put("/updatelistroom/:id", listRoomController.updatelistRoom);
 
 module.exports = Router;
