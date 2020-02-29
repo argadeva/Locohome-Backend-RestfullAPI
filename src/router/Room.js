57fefb58faa284c67579073e05abd59262978c10
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-02-29 12:51:19 +0700

    add route

--- src/router/Room.js
+++ src/router/Room.js
@@ -1,14 +1,12 @@
-const Express = require('express');
-const Multer = require('multer');
+const Express = require("express");
+const Multer = require("multer");
 
 const Router = Express.Router();
-const RoomController = require('../controllers/room');
+const RoomController = require("../controllers/room");
 
-
-Router
-.get('/', RoomController.getRoom)
-.delete('/delete/:id', RoomController.deleteRoom)
-.post('/insert', RoomController.isertRoom)
-.put('/update/:id', RoomController.updateRoom)
+Router.get("/", RoomController.getRoom)
+  .delete("/delete/:id", RoomController.deleteRoom)
+  .post("/insert", RoomController.isertRoom)
+  .put("/update/:id", RoomController.updateRoom);
 
 module.exports = Router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const orders = require("./orders");
 const room = require("./room");
 const listroom = require("./listroom");
 
 const Router = express.Router();
 
+Router.use("/orders", orders);
 Router.use("/room", room);
 Router.use("/listroom", listroom);
 
