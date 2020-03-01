6fa8c7e4cc8b4166d57b41b326deda3140ff57be
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-03-01 23:23:58 +0700

    fix login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -130,6 +130,10 @@ module.exports = {
           res.json({
             token: token
           });
+        } else {
+          res.json({
+            token: null
+          });
         }
       })
       .catch(err => console.log(err));
