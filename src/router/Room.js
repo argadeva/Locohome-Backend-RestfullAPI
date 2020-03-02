253bfc28e4d1ead0e2a1495dc8ff572dc5463610
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-03-03 00:05:06 +0700

    fix detail

--- src/router/Room.js
+++ src/router/Room.js
@@ -1,13 +1,13 @@
 const Express = require("express");
-const auth = require('../helpers/auth')
+const auth = require("../helpers/auth");
 const Router = Express.Router();
 const RoomController = require("../controllers/room");
 
-Router.get("/",auth.verify, RoomController.getRoom)
-  .delete("/delete/:id",auth.verify, RoomController.deleteRoom)
-  .post("/insert",auth.verify, RoomController.isertRoom)
-  .put("/update/:id",auth.verify, RoomController.updateRoom)
-  .get("/search",auth.verify, RoomController.searchRoom)
-  .get("/detail",auth.verify, RoomController.detailRoom);
+Router.get("/", auth.verify, RoomController.getRoom);
+Router.delete("/delete/:id", auth.verify, RoomController.deleteRoom);
+Router.post("/insert", auth.verify, RoomController.isertRoom);
+Router.put("/update/:id", auth.verify, RoomController.updateRoom);
+Router.get("/search", auth.verify, RoomController.searchRoom);
+Router.get("/details", auth.verify, RoomController.detailRoom);
 
 module.exports = Router;
