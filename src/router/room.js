d53e270e870bdb24789c3914d9e28b061e7c8fdf
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-03-03 22:56:18 +0700

    fix token

--- src/router/room.js
+++ src/router/room.js
@@ -3,14 +3,12 @@ const auth = require("../helpers/auth");
 const Router = Express.Router();
 const RoomController = require("../controllers/room");
 
-//dddd
-
 Router.get("/", auth.verify, RoomController.getRoom);
 Router.get("/detail/:id", auth.verify, RoomController.detailRoom);
 Router.delete("/delete/:id", auth.verify, RoomController.deleteRoom);
 Router.post("/insert", auth.verify, RoomController.isertRoom);
 Router.put("/update/:id", auth.verify, RoomController.updateRoom);
 Router.get("/search", auth.verify, RoomController.searchRoom);
-Router.get("/searchall", auth.verify, RoomController.searchAll);
+Router.get("/searchall", RoomController.searchAll);
 
 module.exports = Router;
